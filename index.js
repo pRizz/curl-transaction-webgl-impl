114c23e522e85c068844397c7bc15ae9f61883c5
Peter Ryszkiewicz <pryszkie@gmail.com>
2018-10-29 14:31:14 -0500

    Added index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const curl = require('curl.lib.js')
+
+const trytesLength = 2673
+const nonceLength = 81
+const trytesLessNonce = trytesLength - nonceLength
+
+async function pow({ trytes, minWeightMagnitude }) {
+    const nonce = await curl.pow({ trytes, minWeight: minWeightMagnitude })
+    return trytes.substr(0, trytesLessNonce).concat(nonce)
+}
+
+let error = null
+
+try {
+    curl.init()
+} catch(e) {
+    error = 'Cannot initialize curl. WebGL is probably unavailable.'
+}
+
+module.exports = {
+    pow,
+    error
+}
